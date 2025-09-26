e5dac6350c8b334885b8a163f06b980572940845
Daisuke <skd.itkn@gmail.com>
2025-09-26 03:38:08 -0700

    axios config done

--- src/axios_config.tsx
+++ src/axios_config.tsx
@@ -5,6 +5,8 @@ const app = axios.create({
   // baseURL: baseUrl,
   headers: {
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Content-Type": "application/json",
   },
   withCredentials: true,
