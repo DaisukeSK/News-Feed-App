e602337fbee536d121266ab23e06972cb8770546
Daisuke <skd.itkn@gmail.com>
2025-10-02 05:59:56 -0700

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      "/random_joke": {
+        target: "*",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
