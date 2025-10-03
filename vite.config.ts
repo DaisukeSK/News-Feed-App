3369eb5e3c3f6bb1308e4c5e3c335b779c896bbf
Daisuke <skd.itkn@gmail.com>
2025-10-03 04:08:47 -0700

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    outDir: "./dist",
+    rollupOptions: {
+      input: {
+        api: path.resolve(__dirname, "src/main.ts"),
+      },
+    },
+    ssr: true,
+  },
+  ssr: {
+    noExternal: true,
+    target: "node",
+  },
   server: {
     proxy: {
       "/api": {
